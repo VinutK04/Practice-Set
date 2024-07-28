e2ea9c85109d282d7d68c513a08ed5b3eff3ccd2
Vinut Kumbar <124273341+VinutK04@users.noreply.github.com>
2024-07-28 13:32:33 +0530

    Update fetchNews.py

--- fetchNews.py
+++ fetchNews.py
@@ -1,21 +1,12 @@
 from GoogleNews import GoogleNews
 
-# Create a GoogleNews object.
 google_news = GoogleNews()
-
-# Fetch local news.
-# Refere functions here
-# https://pypi.org/project/GoogleNews/
-
 
 local_news = google_news.search('local news in Hubli')
 local_news = google_news.get_page(1)
 local_news = google_news.result()
-
-
 # Get results will return the list, 
 # [{'title': '...', 'media': '...', 'date': '...', 'datetime': '...', 'desc': '...', 'link': '...', 'img': '...'}]
-
 if local_news is not None:
   for article in local_news:
     print(article['title'])
