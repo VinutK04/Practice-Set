e47b9e1614f2cbea391ab472406c7f61214a1d66
Vinut Kumbar <124273341+VinutK04@users.noreply.github.com>
2024-07-28 13:26:46 +0530

    Create Access_private_variable.py

---
+++ OOPs-Practice/Access_private_variable.py
@@ -0,0 +1,16 @@
+class Athlete:
+    def __init__(self,name,gender):
+        self.__name = name
+        self.__gender = gender
+    def getrunning(self):
+        if(self.__gender=="girl"):
+            print("150mtr running")
+        else:
+            print("200mtr running")
+
+    def setrunning(self):
+        print(self.__name,"won the race in",self.__gender,"'s tornament")
+
+A1 = Athlete("Maria", "girl")
+A1.getrunning()
+A1.setrunning()
