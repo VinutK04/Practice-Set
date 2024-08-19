0ca0b31c276d862b81316350bafcf8f1b18c5b9b
Vinut Kumbar <124273341+VinutK04@users.noreply.github.com>
2024-08-19 09:58:30 +0530

    Create myRangeFunction.py

---
+++ myRangeFunction.py
@@ -0,0 +1,25 @@
+def my_range(start=None, stop=None, step=None):
+    if stop == None and step == None:
+        stop = start
+        counter = 0
+        while counter < stop:
+            yield counter
+            counter += 1
+    
+    if start != None and stop != None and step == None:
+        while start < stop:
+            yield start
+            start += 1
+    
+    if start != None and stop != None and step != None:
+        while start < stop:
+            yield start
+            start += step
+    
+res = list(my_range(10))
+res2 = list(my_range(2, 20))
+res3 = list(my_range(3, 30, 3))
+
+print(res)
+print(res2)
+print(res3)
