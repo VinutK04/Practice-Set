ed0bebd2e376bdbd9b9c4a1777479ad6f762d5f2
Vinut Kumbar <124273341+VinutK04@users.noreply.github.com>
2024-08-19 10:02:46 +0530

    Update myRangeFunction.py

--- myRangeFunction.py
+++ myRangeFunction.py
@@ -15,11 +15,7 @@ def my_range(start=None, stop=None, step=None):
         while start < stop:
             yield start
             start += step
-    
-res = list(my_range(10))
-res2 = list(my_range(2, 20))
-res3 = list(my_range(3, 30, 3))
 
-print(res)
-print(res2)
-print(res3)
+print(list(my_range(10)))
+print(list(my_range(2, 20)))
+print(list(my_range(3, 30, 3)))
