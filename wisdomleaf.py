8f86d1f89bcba1ae96856e39880b5132f98a9935
Vinut Kumbar <124273341+VinutK04@users.noreply.github.com>
2025-01-09 19:20:22 +0530

    Update wisdomleaf.py
    updated remaining two code

--- wisdomleaf.py
+++ wisdomleaf.py
@@ -13,3 +13,34 @@ for idx, val in enumerate(nums):
     seen.add(val)  # Add the current value to the set
 
 print(res)
+
+# 2nd Question
+def isValid(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping.keys():
+            if stack == [] or mapping[char] != stack.pop():
+                return False
+    return stack == []
+
+givenStr = "()[]{"
+print(isValid(givenStr))
+
+# 3rd Question
+def topKFrequent(nums, k):
+    count = {}
+    for num in nums:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+
+    sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+    return [x[0] for x in sorted_count[:k]]
+    
+nums = [1,1,1,2,2,3]
+k = 2
+print(topKFrequent(nums, k))
